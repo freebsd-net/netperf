2e948031b7ed99dc9d5f9612cb9acaac87fa5d7d
George Neville-Neil <gnn@freebsd.org>
2015-02-20 01:55:57 -0500

    Update configs and scripts for a Chelsio based DUT (rabbit3)

--- IPFW/Configs/ipfwclear.sh
+++ IPFW/Configs/ipfwclear.sh
@@ -1,7 +1,7 @@
 # ipfw rules script to clear all rules and install an any to any
 ipfw -q -f flush       # Delete all rules
 # Set defaults
-control="em0"          # control interface
+control="igb0"          # control interface
 in="ix0"
 out="ix1"
 cmd="ipfw -q add "     # build rule prefix
--- IPFW/Configs/ipfwpassDNS.sh
+++ IPFW/Configs/ipfwpassDNS.sh
@@ -1,10 +1,10 @@
 # ipfw rules script to clear all rules and install an any to any
 ipfw -q -f flush       # Delete all rules
 # Set defaults
-control="em0"          # control interface
+control="igb0"          # control interface
 loop="lo0"
-in="ix0"
-out="ix1"
+in="cxl0"
+out="cxl1"
 cmd="ipfw -q add "     # build rule prefix
 ks="keep-state"        # just too lazy to key this each time
 
@@ -14,7 +14,7 @@ $cmd 00005 allow all from any to any via $control
 # No restrictions on Loopback Interface
 $cmd 00010 allow all from any to any via $loop
 
-# Pass only UDP packets between ix0 and ix1
+# Pass only UDP packets between cxl0 and cxl1
 
 $cmd 00500 allow udp from any to any 53 in via $in
 $cmd 00500 allow udp from any to any 53 out via $out
